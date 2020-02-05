bca7d5c118db24a187053f74835f344da57cecf0
Jodi Pranata <jodranata@gmail.com>
2020-02-05 17:43:01 +0700

    Updates

---
+++ precache-manifest.68c4e1da176515d976649e843c23b7b2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9f3c72e4458caa785ba23e4aebd649",
+    "url": "/FCC-drummachine/index.html"
+  },
+  {
+    "revision": "a892f6bf5ed3c4246f0f",
+    "url": "/FCC-drummachine/static/css/main.d26c2f14.chunk.css"
+  },
+  {
+    "revision": "86547d8480458d857b8a",
+    "url": "/FCC-drummachine/static/js/2.b04d56f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FCC-drummachine/static/js/2.b04d56f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a892f6bf5ed3c4246f0f",
+    "url": "/FCC-drummachine/static/js/main.1b2b939b.chunk.js"
+  },
+  {
+    "revision": "16c403e382bd1c054a00",
+    "url": "/FCC-drummachine/static/js/runtime-main.ffda60d3.js"
+  },
+  {
+    "revision": "5822ba4297845dfd5faaf9b57eaa3138",
+    "url": "/FCC-drummachine/static/media/backgrounddrum.5822ba42.jpg"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/FCC-drummachine/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/FCC-drummachine/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/FCC-drummachine/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/FCC-drummachine/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/FCC-drummachine/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/FCC-drummachine/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/FCC-drummachine/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/FCC-drummachine/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/FCC-drummachine/static/media/tom.572ed39d.wav"
+  }
+]);
